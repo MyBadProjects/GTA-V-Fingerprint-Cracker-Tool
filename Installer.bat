b2a91fbcc0494712f4488688eef85daa4947b29c
backspace <60071719+MyBadProjects@users.noreply.github.com>
2021-02-08 20:00:17 +0000

    Add files via upload

---
+++ Installer.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+ECHO Installing Packages
+ECHO Installing PyAutoGUI (0.9.52)
+pip install pyautogui==0.9.52
+ECHO Installed PyAutoGUI (0.9.52)
+ECHO Installing wxPython (4.1.1)
+pip install wxPython==4.1.1
+ECHO Installed wxPython (4.1.1)
+ECHO Installing keyboard (0.13.5)
+pip install keyboard==0.13.5
+ECHO Installed keyboard (0.13.5)
+ECHO Downloading Git Clone/Repo
+git clone https://github.com/MyBadProjects/GTA-V-Fingerprint-Cracker-Tool
+ECHO Downloaded Git Clone/Repo
+ECHO Finished Installing!
+PAUSE
+EXIT
